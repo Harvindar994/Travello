dcfc9c174907b853446417e44241edc2b6e5bd3c
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2021-06-21 17:18:09 +0530

    Registration Form Creating

--- travello/accounts/views.py
+++ travello/accounts/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
 
 def registration(request):
-    return HttpResponse('registration Page Working....')
+    if request.method == 'POST':
+        frist_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        confirmPassword = request.POST['confirmPassword']
+
+        user = User.objects.create_user(username=username, password=password, email=email, first_name=frist_name, last_name=last_name)
+        user.save()
+        print("User Created")
+        return redirect('/')
+    else:
+        return render(request, 'registration.html')
