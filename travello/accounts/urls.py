97236adf63aebcafcda1899652f2d0d015f458a2
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2021-06-22 04:25:21 +0530

    Login And registration is working

--- travello/accounts/urls.py
+++ travello/accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('registration', views.registration, name='registration'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout')
 ]
--- travello/accounts/views.py
+++ travello/accounts/views.py
@@ -15,9 +15,39 @@ def registration(request):
         password = request.POST['password']
         confirmPassword = request.POST['confirmPassword']
 
-        user = User.objects.create_user(username=username, password=password, email=email, first_name=frist_name, last_name=last_name)
-        user.save()
-        print("User Created")
-        return redirect('/')
+        if confirmPassword == password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, "username already used.")
+                return redirect('registration')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, "email already exits.")
+                return redirect('registration')
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email, first_name=frist_name,
+                                                last_name=last_name)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, "password is not matching.")
+            return redirect('registration')
     else:
         return render(request, 'registration.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'username or password is invalid')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
