a84ca1d2b0fca79c0d85325aa89ab2a60aaec931
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2021-06-24 07:00:53 +0530

    Now Creating E element For Dynamic content

--- travello/Application/urls.py
+++ travello/Application/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about', views.about, name='about')
+    path('about.html', views.about, name='about'),
+    path('contact.html', views.about, name='contact'),
+    path('element.html', views.about, name='element'),
+    path('news.html', views.about, name='news'),
+    path('destinations.html', views.about, name='destination'),
 ]
